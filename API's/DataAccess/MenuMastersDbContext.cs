6157d0e4ac58d7e63165ed214626b47a06812302
Bas Smit <bas.smit@live.nl>
2023-11-27 15:25:14 +0100

    Update MenuMastersDbContext.cs

--- API's/DataAccess/MenuMastersDbContext.cs
+++ API's/DataAccess/MenuMastersDbContext.cs
@@ -24,7 +24,7 @@ namespace DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost; Database=menumasters; User=sa; Password=Admin123; Encrypt=False");
+            optionsBuilder.UseSqlServer("Server=PC-Bas; Database=menumasters; Trusted_Connection=True; Encrypt=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
