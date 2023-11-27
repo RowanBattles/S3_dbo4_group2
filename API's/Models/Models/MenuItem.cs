a0dc5b8348b6a01b22cbca7d249166d13f542b08
Rowan <rowanvanderweel@gmail.com>
2023-11-27 16:17:14 +0100

    endpoint menuItem fixed

--- API's/Models/Models/MenuItem.cs
+++ API's/Models/Models/MenuItem.cs
@@ -8,23 +8,23 @@ namespace Models
 
 		public string ItemName { get; set; }
 
-		public string ItemDescription_Short { get; set; }
+		public string? ItemDescription_Short { get; set; }
 
-        public string ItemDescription_Long { get; set; }
+        public string? ItemDescription_Long { get; set; }
 
         public decimal ItemPrice { get; set; }
 
-		public int ItemStock { get; set; }
+		public int? ItemStock { get; set; }
 
 		public int CategoryId { get; set; }
 
 		public Category? Category { get; set; } = null!;
 
-        public string ImageURL { get; set; }
+        public string? ImageURL { get; set; }
 
-		public string DietaryInfo { get; set; }
+		public string? DietaryInfo { get; set; }
 
-		public string Ingredients { get; set; }
+		public string? Ingredients { get; set; }
     }
 }
 
