ec0d83bbb124ac38856ab6243b2070f831d25fae
NvSchaijk <77079918+NvSchaijk@users.noreply.github.com>
2023-11-28 13:34:51 +0100

    +1 tiny change

--- API_Menu/Menumasters_MenuAPI/MenuAPI_Models/enums/OrderStatus.cs
+++ API_Menu/Menumasters_MenuAPI/MenuAPI_Models/enums/OrderStatus.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 namespace MenuAPI_Models.enums
 {
-	public class OrderStatus
+	public enum OrderStatus : byte
 	{
-		public enum OrderStatus : byte
-		{
-            Pending = 0,
-			Active = 1,
-			Completed = 2
+        Pending = 0,
+		Active = 1,
+		Completed = 2
 
-        }
-	}
+    }
 }
 
