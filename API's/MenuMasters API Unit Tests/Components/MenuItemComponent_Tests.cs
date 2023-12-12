ecb4493fec6afe3ced211ffa9d7a98fc0f8ad0dd
Sascha Adriaens <s.adriaens@student.fontys.nl>
2023-12-12 13:27:36 +0100

    Finish easy components

--- API's/MenuMasters API Unit Tests/Components/MenuItemComponent_Tests.cs
+++ API's/MenuMasters API Unit Tests/Components/MenuItemComponent_Tests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Bussiness.Components;
+using Contract_API_Bussiness.Interfaces;
 using Contract_Data_Bussiness.Interfaces;
+using Models;
+using Models.Enums;
 using Moq;
 
 namespace MenuMasters_API_Unit_Tests.Components
@@ -23,31 +26,146 @@ namespace MenuMasters_API_Unit_Tests.Components
         [Fact]
         public async Task GetAllMenuItemsAsync_Test()
         {
+            // Arrange
+            IEnumerable<MenuItem> expectedMenuItems = new List<MenuItem>()
+            {
+                new MenuItem
+                {
+                    MenuItemId = 1,
+                    ItemName = "Burger",
+                    ItemPrice = 3.50m,
+                    ItemDescription_Long = "A classic burger",
+                    ItemDescription_Short = "A classic burger",
+                    ItemStock = 20
+                }
+            };
 
+            _menuItemRepositoryMock
+                .Setup(repo => repo.GetAllMenuItemsAsync())
+                .ReturnsAsync(expectedMenuItems);
+
+            // Act
+            IEnumerable<MenuItem> result = await _menuItemComponent.GetAllMenuItemsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedMenuItems, result);
         }
 
         [Fact]
         public async Task GetMenuItemByIdAsync_Test()
         {
+            // Arrange
+            int inputMenuItemId = 1;
 
+            MenuItem expectedMenuItem = new MenuItem
+            {
+                MenuItemId = 1,
+                ItemName = "Burger",
+                ItemPrice = 3.50m,
+                ItemDescription_Long = "A classic burger",
+                ItemDescription_Short = "A classic burger",
+                ItemStock = 20
+            };
+
+            _menuItemRepositoryMock
+                .Setup(repo => repo.GetMenuItemByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(expectedMenuItem);
+
+            // Act
+            MenuItem? result = await _menuItemComponent.GetMenuItemByIdAsync(inputMenuItemId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedMenuItem, result);
         }
 
         [Fact]
         public async Task CreateMenuItemAsync_Test()
         {
+            // Arrange
+            MenuItem expectedMenuItem = new MenuItem
+            {
+                MenuItemId = 1,
+                ItemName = "Burger",
+                ItemPrice = 3.50m,
+                ItemDescription_Long = "A classic burger",
+                ItemDescription_Short = "A classic burger",
+                ItemStock = 20
+            };
 
+            MenuItem inputMenuItem = new MenuItem
+            {
+                ItemName = "Burger",
+                ItemPrice = 3.50m,
+                ItemDescription_Long = "A classic burger",
+                ItemDescription_Short = "A classic burger",
+                ItemStock = 20
+            };
+
+            _menuItemRepositoryMock
+                .Setup(repo => repo.CreateMenuItemAsync(It.IsAny<MenuItem>()))
+                .ReturnsAsync(expectedMenuItem);
+
+            // Act
+            MenuItem? result = await _menuItemComponent.CreateMenuItemAsync(inputMenuItem);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedMenuItem, result);
         }
 
         [Fact]
         public async Task UpdateMenuItemAsync_Test()
         {
+            // Arrange
+            MenuItem expectedMenuItem = new MenuItem
+            {
+                MenuItemId = 1,
+                ItemName = "Hamburger",
+                ItemPrice = 4.50m,
+                ItemDescription_Long = "A classic burger",
+                ItemDescription_Short = "A classic burger",
+                ItemStock = 20
+            };
 
+            MenuItem inputMenuItem = new MenuItem
+            {
+                MenuItemId = 1,
+                ItemName = "Hamburger",
+                ItemPrice = 4.50m,
+                ItemDescription_Long = "A classic burger",
+                ItemDescription_Short = "A classic burger",
+                ItemStock = 20
+            };
+
+            _menuItemRepositoryMock
+                .Setup(repo => repo.UpdateMenuItemAsync(It.IsAny<MenuItem>()))
+                .ReturnsAsync(expectedMenuItem);
+
+            // Act
+            MenuItem? result = await _menuItemComponent.UpdateMenuItemAsync(inputMenuItem);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedMenuItem, result);
         }
 
         [Fact]
         public async Task DeleteMenuItemAsync_Test()
         {
+            // Arrange
+            int inputMenuItemId = 1;
 
+            _menuItemRepositoryMock
+                .Setup(repo => repo.DeleteMenuItemAsync(It.IsAny<int>()))
+                .ReturnsAsync(true);
+
+            // Act
+            bool result = await _menuItemComponent.DeleteMenuItemAsync(inputMenuItemId);
+
+            // Assert
+            Assert.True(result);
         }
     }
 }
