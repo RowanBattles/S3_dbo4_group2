b441aa65b813c67268852bdd99ebb371de56656d
Sascha Adriaens <s.adriaens@student.fontys.nl>
2023-12-12 13:01:24 +0100

    Make unit tests for all repositories

--- API's/MenuMasters API Unit Tests/Repositories/CategoryRepository_Tests.cs
+++ API's/MenuMasters API Unit Tests/Repositories/CategoryRepository_Tests.cs
@@ -1,37 +1,119 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccess;
+using DataAccess.Repositories;
+using Models;
+using Models.Enums;
 
 namespace MenuMasters_API_Unit_Tests.Repositories
 {
     public class CategoryRepository_Tests
     {
+        private readonly DbContextOptions<MenuMastersDbContext> _options;
+
+        public CategoryRepository_Tests()
+        {
+            _options = new DbContextOptionsBuilder<MenuMastersDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{new Random().Next(0, 100)}")
+                .Options;
+        }
+
         [Fact]
         public async Task GetAllCategoriesAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                CategoryRepository categoryRepository = new CategoryRepository(dbContext);
+                dbContext.Categories.Add(new Category { CategoryName = "Soft Drinks", Type = OrderType.Bar });
+                dbContext.Categories.Add(new Category { CategoryName = "Burgers", Type = OrderType.Kitchen });
+                dbContext.SaveChanges();
 
+                // Act
+                IEnumerable<Category> result = await categoryRepository.GetAllCategoriesAsync();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(2, result.Count());
+                Assert.Contains(result, x => x.CategoryName == "Soft Drinks");
+                Assert.Contains(result, x => x.CategoryName == "Burgers");
+            }
         }
 
         [Fact]
         public async Task GetCategoryByIdAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                CategoryRepository categoryRepository = new CategoryRepository(dbContext);
+                dbContext.Categories.Add(new Category { CategoryName = "Soft Drinks", Type = OrderType.Bar });
+                dbContext.SaveChanges();
 
+                // Act
+                Category? result = await categoryRepository.GetCategoryByIdAsync(1);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Soft Drinks", result.CategoryName);
+                Assert.Equal(OrderType.Bar, result.Type);
+            }
         }
 
         [Fact]
         public async Task CreateCategoryAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                CategoryRepository categoryRepository = new CategoryRepository(dbContext);
+                Category newCategory = new Category { CategoryName = "Soft Drinks", Type = OrderType.Bar };
 
+                // Act
+                Category? result = await categoryRepository.CreateCategoryAsync(newCategory);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Soft Drinks", result.CategoryName);
+                Assert.Equal(OrderType.Bar, result.Type);
+            }
         }
 
         [Fact]
         public async Task UpdateCategoryAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                CategoryRepository categoryRepository = new CategoryRepository(dbContext);
+                dbContext.Categories.Add(new Category { CategoryName = "Soft Drinks", Type = OrderType.Bar });
+                dbContext.SaveChanges();
+                Category updated = new Category { CategoryId = 1, CategoryName = "Drinks", Type = OrderType.Bar };
 
+                // Act
+                Category? result = await categoryRepository.UpdateCategoryAsync(updated);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Drinks", result.CategoryName);
+            }
         }
 
         [Fact]
         public async Task DeleteCategoryAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                CategoryRepository categoryRepository = new CategoryRepository(dbContext);
+                dbContext.Categories.Add(new Category { CategoryName = "Soft Drinks", Type = OrderType.Bar });
+                dbContext.SaveChanges();
 
+                // Act
+                bool result = await categoryRepository.DeleteCategoryAsync(1);
+
+                // Assert
+                Assert.True(result);
+            }
         }
     }
 }
--- API's/MenuMasters API Unit Tests/Repositories/MenuItemRepository_Tests.cs
+++ API's/MenuMasters API Unit Tests/Repositories/MenuItemRepository_Tests.cs
@@ -1,37 +1,119 @@
-﻿using System;
+﻿using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace MenuMasters_API_Unit_Tests.Repositories
 {
 	public class MenuItemRepository_Tests
 	{
-		[Fact]
+        private readonly DbContextOptions<MenuMastersDbContext> _options;
+
+        public MenuItemRepository_Tests()
+        {
+            _options = new DbContextOptionsBuilder<MenuMastersDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{new Random().Next(0, 100)}")
+                .Options;
+        }
+
+        [Fact]
 		public async Task GetAllMenuItemsAsync_Test()
 		{
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                MenuItemRepository menuItemRepository = new MenuItemRepository(dbContext);
+                dbContext.MenuItems.Add(new MenuItem { ItemName = "Cola", ItemPrice = 1.00m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 });
+                dbContext.MenuItems.Add(new MenuItem { ItemName = "Burger", ItemPrice = 3.50m, ItemDescription_Long = "A classic burger", ItemDescription_Short = "A classic burger", ItemStock = 20 });
+                dbContext.SaveChanges();
 
-		}
+                // Act
+                IEnumerable<MenuItem> result = await menuItemRepository.GetAllMenuItemsAsync();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(2, result.Count());
+                Assert.Contains(result, x => x.ItemName == "Cola");
+                Assert.Contains(result, x => x.ItemName == "Burger");
+            }
+        }
 
         [Fact]
         public async Task GetMenuItemByIdAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                MenuItemRepository menuItemRepository = new MenuItemRepository(dbContext);
+                dbContext.MenuItems.Add(new MenuItem { ItemName = "Cola", ItemPrice = 1.00m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 });
+                dbContext.SaveChanges();
 
+                // Act
+                MenuItem? result = await menuItemRepository.GetMenuItemByIdAsync(1);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Cola", result.ItemName);
+                Assert.Equal(1.00m, result.ItemPrice);
+            }
         }
 
         [Fact]
         public async Task CreateMenuItemAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                MenuItemRepository menuItemRepository = new MenuItemRepository(dbContext);
+                MenuItem newMenuItem = new MenuItem { ItemName = "Cola", ItemPrice = 1.00m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 };
 
+                // Act
+                MenuItem? result = await menuItemRepository.CreateMenuItemAsync(newMenuItem);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Cola", result.ItemName);
+                Assert.Equal(1.00m, result.ItemPrice);
+            }
         }
 
         [Fact]
         public async Task UpdateMenuItemAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                MenuItemRepository menuItemRepository = new MenuItemRepository(dbContext);
+                dbContext.MenuItems.Add(new MenuItem { ItemName = "Cola", ItemPrice = 1.00m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 });
+                dbContext.SaveChanges();
+                MenuItem updated = new MenuItem { MenuItemId = 1, ItemName = "Sprite", ItemPrice = 1.50m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 };
 
+                // Act
+                MenuItem? result = await menuItemRepository.UpdateMenuItemAsync(updated);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Sprite", result.ItemName);
+                Assert.Equal(1.50m, result.ItemPrice);
+            }
         }
 
         [Fact]
         public async Task DeleteMenuItemAsync_Test()
         {
+            using (MenuMastersDbContext dbContext = new MenuMastersDbContext(_options))
+            {
+                // Arrange
+                MenuItemRepository menuItemRepository = new MenuItemRepository(dbContext);
+                dbContext.MenuItems.Add(new MenuItem { ItemName = "Cola", ItemPrice = 1.00m, ItemDescription_Long = "A refreshing drink", ItemDescription_Short = "A refreshing drink", ItemStock = 10 });
+                dbContext.SaveChanges();
 
+                // Act
+                bool result = await menuItemRepository.DeleteMenuItemAsync(1);
+
+                // Assert
+                Assert.True(result);
+            }
         }
     }
 }
