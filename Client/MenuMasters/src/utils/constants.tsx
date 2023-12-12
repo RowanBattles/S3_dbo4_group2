d8e8840f321564a3ff5bf75ee185a26282737c20
Bas Smit <bas.smit@live.nl>
2023-12-12 11:38:06 +0100

    fixed constants url

--- Client/MenuMasters/src/utils/constants.tsx
+++ Client/MenuMasters/src/utils/constants.tsx
@@ -1,15 +1,16 @@
-const URL = "https://localhost:7266";
+const URL_MENU = "https://localhost:7266";
+const URL_STAFF = "http://localhost:7244";
 
 const ENDPOINTS = {
-  GetAllItems: `${URL}/api/Menu`,
-  GetAllTabs: `${URL}/api/tab/sales`,
-  GetAllOrdersKitchen: `${URL}/api/order/kitchen`,
-  GetAllOrdersBar: `${URL}/api/order/bar`,
-  GetAllCategories: `${URL}/api/Category`,
-  GetItemById: (id: number) => `${URL}/api/Menu/${id}`,
-  GetCategoryById: (id: number) => `${URL}/api/Category/${id}`,
-  CreateOrder: `${URL}/api/Order`,
-  PayTab: `${URL}/api/Tab/Pay`,
+  GetAllItems: `${URL_MENU}/api/Menu`,
+  GetAllTabs: `${URL_STAFF}/api/tab/sales`,
+  GetAllOrdersKitchen: `${URL_STAFF}/api/order/kitchen`,
+  GetAllOrdersBar: `${URL_STAFF}/api/order/bar`,
+  GetAllCategories: `${URL_MENU}/api/Category`,
+  GetItemById: (id: number) => `${URL_MENU}/api/Menu/${id}`,
+  GetCategoryById: (id: number) => `${URL_MENU}/api/Category/${id}`,
+  CreateOrder: `${URL_MENU}/api/Order`,
+  PayTab: `${URL_STAFF}/api/Tab/Pay`,
 };
 
 export default ENDPOINTS;
