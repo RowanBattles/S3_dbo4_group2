a82b05b938d6c9eb042c4c23b6556b28415cda5c
Rowan <117738292+RowanBattles@users.noreply.github.com>
2023-12-18 11:03:56 +0100

    Got for now everything working (#24)
    @sascha-fontys
    Een ding wat nog niet goed werkt, als ik een order post voor een tafel die al bestaat maakt ie alsnog een nieuwe tab aan.

--- Client/MenuMasters/src/components/Staff/Modals/DeleteModal.tsx
+++ Client/MenuMasters/src/components/Staff/Modals/DeleteModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { OrderItemSales, TabEntity } from "../../../types/types";
+import { DeleteProducts } from "../../../utils/api";
 
 interface PayModalProps {
   tab: TabEntity;
@@ -19,28 +20,67 @@ const PayModal: React.FC<PayModalProps> = ({ tab, onClose }) => {
   };
 
   const handleOrderItemClick = (clickedItem: OrderItemSales) => {
-    const updatedOrderedItems = OrderedItems.map((item) =>
-      item.orderItemId === clickedItem.orderItemId
-        ? { ...item, quantity: Math.max(0, item.quantity - 1) }
-        : item
-    ).filter((item) => item.quantity > 0);
+    if (clickedItem.quantity > 0) {
+      const updatedOrderedItems = OrderedItems.map((item) =>
+        item.orderItemId === clickedItem.orderItemId
+          ? { ...item, quantity: Math.max(0, item.quantity - 1) }
+          : item
+      );
 
-    const existingCorrectionItemIndex = CorrectionItems.findIndex(
+      const existingCorrectionItemIndex = CorrectionItems.findIndex(
+        (item) => item.orderItemId === clickedItem.orderItemId
+      );
+
+      if (existingCorrectionItemIndex !== -1) {
+        const updatedCorrectionItems = [...CorrectionItems];
+        updatedCorrectionItems[existingCorrectionItemIndex].quantity += 1;
+        setCorrectionItems(updatedCorrectionItems);
+      } else {
+        setCorrectionItems((prevCorrectionItems) => [
+          ...prevCorrectionItems,
+          { ...clickedItem, quantity: 1 },
+        ]);
+      }
+
+      setOrderedItems(updatedOrderedItems);
+    }
+  };
+
+  const handleCorrectionItemClick = (clickedItem: OrderItemSales) => {
+    const existingCorrectionItem = CorrectionItems.find(
       (item) => item.orderItemId === clickedItem.orderItemId
     );
 
-    if (existingCorrectionItemIndex !== -1) {
-      const updatedCorrectionItems = [...CorrectionItems];
-      updatedCorrectionItems[existingCorrectionItemIndex].quantity += 1;
-      setCorrectionItems(updatedCorrectionItems);
-    } else {
-      setCorrectionItems((prevCorrectionItems) => [
-        ...prevCorrectionItems,
-        { ...clickedItem, quantity: 1 },
-      ]);
-    }
+    if (existingCorrectionItem?.quantity && clickedItem.quantity >= 0) {
+      const updatedCorrectionItems = CorrectionItems.map((item) =>
+        item.orderItemId === clickedItem.orderItemId
+          ? { ...item, quantity: Math.max(0, item.quantity - 1) }
+          : item
+      );
 
-    setOrderedItems(updatedOrderedItems);
+      const updatedOrderedItems = [...OrderedItems];
+      const existingOrderedItemIndex = OrderedItems.findIndex(
+        (item) => item.orderItemId === clickedItem.orderItemId
+      );
+      updatedOrderedItems[existingOrderedItemIndex].quantity += 1;
+
+      setCorrectionItems(updatedCorrectionItems);
+      setOrderedItems(updatedOrderedItems);
+    }
+  };
+
+  const handleCorrectionClick = async () => {
+    const itemsToUpdate = OrderedItems.map(({ orderItemId, quantity }) => ({
+      orderItemId,
+      quantity,
+    }));
+
+    try {
+      await DeleteProducts(itemsToUpdate);
+      handleCloseClick();
+    } catch (error) {
+      console.error("error proceeding deleting products: ", error);
+    }
   };
 
   return (
@@ -63,7 +103,9 @@ const PayModal: React.FC<PayModalProps> = ({ tab, onClose }) => {
                 <li
                   key={item.orderItemId}
                   onClick={() => handleOrderItemClick(item)}
-                  className="yellow relative rounded-2xl text-2xl h-[200px] text-white cursor-pointer"
+                  className={`yellow relative rounded-2xl text-2xl h-[200px] text-white cursor-pointer ${
+                    item.quantity == 0 ? "hidden" : ""
+                  }`}
                 >
                   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 w-full px-2 -translate-y-1/2 text-center break-keep line-clamp-2">
                     {item.itemName}
@@ -80,8 +122,11 @@ const PayModal: React.FC<PayModalProps> = ({ tab, onClose }) => {
               <ul className="grid-cols-3 max-h-full overflow-y-auto grid gap-5">
                 {CorrectionItems.map((item) => (
                   <li
+                    onClick={() => handleCorrectionItemClick(item)}
                     key={item.orderItemId}
-                    className="yellow relative rounded-2xl text-2xl h-[200px] text-white cursor-pointer"
+                    className={`yellow relative rounded-2xl text-2xl h-[200px] text-white cursor-pointer ${
+                      item.quantity == 0 ? "hidden" : ""
+                    }`}
                   >
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 w-full px-2 -translate-y-1/2 text-center break-keep line-clamp-2">
                       {item.itemName}
@@ -94,7 +139,10 @@ const PayModal: React.FC<PayModalProps> = ({ tab, onClose }) => {
               </ul>
             </div>
             <div className="h-1/6 pt-2">
-              <button className="h-full w-full red rounded-full text-4xl font-bold flex justify-center items-center gap-5">
+              <button
+                className="h-full w-full red rounded-full text-4xl font-bold flex justify-center items-center gap-5"
+                onClick={handleCorrectionClick}
+              >
                 <span>Correction</span>
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/0/340.png"
