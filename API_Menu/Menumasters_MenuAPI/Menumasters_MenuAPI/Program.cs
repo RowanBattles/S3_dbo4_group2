a58d29583a1dfdabc488123d2d292dbf58dd527e
Rowan <rowanvanderweel@gmail.com>
2024-01-08 11:23:40 +0100

    connectionstring
    i18n doesn't work

--- API_Menu/Menumasters_MenuAPI/Menumasters_MenuAPI/Program.cs
+++ API_Menu/Menumasters_MenuAPI/Menumasters_MenuAPI/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnectionRowan");
 builder.Services.AddDbContext<MenuAPIDbContext>(o => o.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<ICategoryDAL, CategoryDAL>();
