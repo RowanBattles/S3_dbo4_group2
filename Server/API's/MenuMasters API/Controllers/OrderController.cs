8ee507b4614163829478af9741551734ea232a3a
Rowan <rowanvanderweel@gmail.com>
2024-01-16 11:41:55 +0100

    Check readme

---
+++ Server/API's/MenuMasters API/Controllers/OrderController.cs
@@ -0,0 +1,118 @@
+﻿using Contract_API_Bussiness.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.DTOs;
+using Models.Enums;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace MenuMasters_API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly ILogger<OrderController> _logger;
+    private readonly IOrderComponent _orderComponent;
+
+    public OrderController(ILogger<OrderController> logger, IOrderComponent orderComponent)
+    {
+        _logger = logger;
+        _orderComponent = orderComponent;
+    }
+
+    [HttpGet(Name = "GetAllOrders")]
+    public async Task<IEnumerable<Order>> Get()
+    {
+        return await _orderComponent.GetAllOrdersAsync();
+    }
+
+    [HttpGet("Kitchen", Name = "GetAllKitchenOrders")]
+    public async Task<IEnumerable<KitchenOrder>> GetKitchen()
+    {
+        return await _orderComponent.GetAllKitchenOrdersAsync();
+    }
+
+    [HttpGet("Bar", Name = "GetAllBarOrders")]
+    public async Task<IEnumerable<KitchenOrder>> GetBar()
+    {
+        return await _orderComponent.GetAllBarOrdersAsync();
+    }
+
+    [HttpGet("{id}", Name = "GetOrderById")]
+    public async Task<Order?> Get(int id)
+    {
+        return await _orderComponent.GetOrderByIdAsync(id);
+    }
+
+    [HttpPost(Name = "PostOrder")]
+    public async Task<IActionResult> Post(PostOrder postOrder)
+    {
+        Order? result = await _orderComponent.CreateOrderAsync(postOrder);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPost("Item", Name = "PostOrderItem")]
+    public async Task<IActionResult> PostOrderItem(OrderItem orderItem)
+    {
+        OrderItem? result = await _orderComponent.AddItemToOrderAsync(orderItem);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPatch(Name = "PatchOrder")]
+    public async Task<IActionResult> Patch(Order order)
+    {
+        Order? result = await _orderComponent.UpdateOrderAsync(order);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPatch("Item", Name = "PatchOrderItems")]
+    public async Task<IActionResult> PatchOrderItem(IEnumerable<PatchOrderItem> orderItems)
+    {
+        List<OrderItem> results = new List<OrderItem>();
+
+        foreach (PatchOrderItem orderItem in orderItems)
+        {
+            OrderItem? result = await _orderComponent.UpdateItemFromOrderAsync(orderItem);
+
+            if (result == null)
+            {
+                return BadRequest(result);
+            }
+            else
+            {
+                results.Add(result);
+            }
+        }
+
+        return Ok(results);
+    }
+
+    [HttpPatch("Status/Kitchen", Name = "PatchKitchenOrderStatus")]
+    public async Task<IActionResult> PatchKitchenOrderState(int id)
+    {
+        OrderStatus? result = await _orderComponent.UpdateKitchenOrderStateAsync(id);
+        return result != null ? Ok(result.ToString()) : BadRequest(result);
+    }
+
+    [HttpPatch("Status/Bar", Name = "PatchBarOrderStatus")]
+    public async Task<IActionResult> PatchBarOrderState(int id)
+    {
+        OrderStatus? result = await _orderComponent.UpdateBarOrderStateAsync(id);
+        return result != null ? Ok(result.ToString()) : BadRequest(result);
+    }
+
+    [HttpDelete(Name = "DeleteOrder")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        bool success = await _orderComponent.DeleteOrderAsync(id);
+        return success ? Ok() : BadRequest();
+    }
+
+    [HttpDelete("Item", Name = "DeleteOrderItem")]
+    public async Task<IActionResult> DeleteOrderItem(int id)
+    {
+        bool success = await _orderComponent.RemoveItemFromOrderAsync(id);
+        return success ? Ok() : BadRequest();
+    }
+}
+
---
+++ Server/API's/MenuMasters API/Controllers/TabController.cs
@@ -0,0 +1,73 @@
+﻿using Contract_API_Bussiness.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.DTOs;
+
+namespace MenuMasters_API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TabController : ControllerBase
+{
+    private readonly ILogger<TabController> _logger;
+    private readonly ITabComponent _tabComponent;
+
+    public TabController(ILogger<TabController> logger, ITabComponent tabComponent)
+    {
+        _logger = logger;
+        _tabComponent = tabComponent;
+    }
+
+    [HttpGet(Name = "GetAllTabs")]
+    public async Task<IEnumerable<Tab>> Get()
+    {
+        return await _tabComponent.GetAllTabsAsync();
+    }
+
+    [HttpGet("Sales", Name = "GetAllSalesTabs")]
+    public async Task<IEnumerable<SalesTab>> GetSales()
+    {
+        return await _tabComponent.GetAllSalesTabsAsync();
+    }
+
+    [HttpGet("{id}", Name = "GetTabById")]
+    public async Task<Tab?> Get(int id)
+    {
+        return await _tabComponent.GetTabByIdAsync(id);
+    }
+
+    [HttpGet("Sales/{id}", Name = "GetSalesTabById")]
+    public async Task<SalesTab?> GetSales(int id)
+    {
+        return await _tabComponent.GetSalesTabByIdAsync(id);
+    }
+
+    [HttpPost(Name = "PostTab")]
+    public async Task<IActionResult> Post(Tab tab)
+    {
+        Tab? result = await _tabComponent.CreateTabAsync(tab);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPatch(Name = "PatchTab")]
+    public async Task<IActionResult> Patch(Tab tab)
+    {
+        Tab? result = await _tabComponent.UpdateTabAsync(tab);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPatch("Pay", Name = "PatchTabPay")]
+    public async Task<IActionResult> PatchPay(PayTab payTab)
+    {
+        SalesTab? result = await _tabComponent.PayTab(payTab);
+        return result != null ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpDelete(Name = "DeleteTab")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        bool success = await _tabComponent.DeleteTabAsync(id);
+        return success ? Ok() : BadRequest();
+    }
+}
+
